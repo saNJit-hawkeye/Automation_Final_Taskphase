a47c0406b220142c78113aaf28974b803881a98f
saNJit-hawkeye <sanjitvignesh2006.yuvabharathi@gmail.com>
2025-03-29 14:48:47 +0530

    1st task basic bot

---
+++ robot_task1/robot_task1/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from robot_task1.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ robot_task1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'robot_task1'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        # Install marker file for package index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # Install package.xml
+        ('share/' + package_name, ['package.xml']),
+        # Install URDF files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        # Install world files
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='Robot Task1 Package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [],
+    },
+)
+
