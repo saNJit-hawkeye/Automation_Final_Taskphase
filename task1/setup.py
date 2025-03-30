2c0f9c5cfee2c2ee91ed5bc939fa4ac169a81e16
saNJit-hawkeye <sanjitvignesh2006.yuvabharathi@gmail.com>
2025-03-30 15:27:10 +0530

    Updated model of the robot along with lidar added to it

--- task1/setup.py
+++ task1/setup.py
@@ -1,25 +1,33 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'task1'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+        # Install marker file for package index
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        # Install package.xml
         ('share/' + package_name, ['package.xml']),
+        # Install URDF files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+        # Install world files
+        (os.path.join('share', package_name, 'worlds'), glob('worlds/*.world')),
+        # Install launch files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='sanjit',
-    maintainer_email='sanjit@todo.todo',
-    description='TODO: Package description',
-    license='Apache-2.0',
+    maintainer='your_name',
+    maintainer_email='your_email@example.com',
+    description='bot Task1 Package',
+    license='Apache License 2.0',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': [
-        ],
+        'console_scripts': [],
     },
 )
